3d4baca18bb840340dcdb602bd8c44f5d38cc01b
Kirk Tolliver <kirktolliver28@gmail.com>
2020-03-15 22:29:38 -0500

    Mvp

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from  'react-router-dom';
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
+	// console.log(props);
   useEffect(() => {
     const getMovies = () => {
       axios
@@ -20,9 +22,14 @@ const MovieList = props => {
   
   return (
     <div className="movie-list">
-      {movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
-      ))}
+      {movies.map(movie => {
+
+	      return (
+	      <Link to={ `/movies/${movie.id}` }>
+		      <MovieDetails key={movie.id} movie={movie} />
+	      </Link>
+	      )
+	    })}
     </div>
   );
 }
@@ -30,22 +37,22 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
+		  <div className="movie-card">
+		  <h2>{title}</h2>
+			  <div className="movie-director">
+			  Director: <em>{director}</em>
+			  </div>
+			  <div className="movie-metascore">
+			  Metascore: <strong>{metascore}</strong>
+			  </div>
+		  <h3>Actors</h3>
 
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
+	  {stars.map(star => (
+			  <div key={star} className="movie-star">
+			  {star}
+		  </div>
+		  ))}
+	  </div>
   );
 }
 
